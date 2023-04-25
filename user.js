40afdca5a842ab9e713739913ee5a6651ab46ded
Arthur Fernandez <arthur@snooget.com>
2023-04-25 09:45:18 -0700

    added three ways to export the classes and functions from user.js to main.js

--- user.js
+++ user.js
@@ -1,14 +1,27 @@
-class user {
+// A third way to export would be inline by writing export default before the class and export before the functions..
+export default class user {
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
 }
 
-function printName(user) {
+export function printName(user) {
     console.log(`User's name is ${user.name}`);
 }
 
-function printAge(user) {
+export function printAge(user) {
     console.log(`User is ${user.age} years old`);
 }
+
+// Ther eare many different ways to export the classes and functions above. 
+
+// The two options are default exports and standard exports.
+// Below is the first way:
+/* export default user */
+//================================================
+// Below is the normal export
+/*
+export default user
+export {printName, printAge};
+*/
